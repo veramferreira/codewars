3f9b91951a34cfc1532c50bd58dfd85167967e0b
Vera Ferreira <verammferreira@gmail.com>
2023-02-28 11:48:42 +0000

    add codewars challenge solution

---
+++ 8kyu/get-mean-of-array.js
@@ -0,0 +1,17 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+// MY SOLUTION
+
+function getAverage(marks) {
+  let totalAv = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    totalAv += marks[i];
+  }
+
+  return Math.floor(totalAv / marks.length);
+}
