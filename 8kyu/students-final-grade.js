52b9eadab54b6e807fa5f827b8247e23f82c765f
Vera Ferreira <115066930+veramferreira@users.noreply.github.com>
2023-03-12 17:47:57 +0000

    "daily codewars challenge"

--- 8kyu/students-final-grade.js
+++ 8kyu/students-final-grade.js
@@ -26,8 +26,8 @@
 function finalGrade (exam, projects) {
   
     return exam > 90 || projects > 10 ? 100
-          : exam > 75 || projects >= 5 ? 90
-          : exam > 50 || projects >= 2 ? 75
+          : exam > 75 & projects >= 5 ? 90
+          : exam > 50 & projects >= 2 ? 75
           : 0
   }
   
