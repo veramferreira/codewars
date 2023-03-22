3338825c7ebad181316748a9ab4d52ac8394774d
Vera Ferreira <115066930+veramferreira@users.noreply.github.com>
2023-03-22 10:50:56 +0000

    'daily codewars challenge'

---
+++ 8kyu/personalized-message.js
@@ -0,0 +1,16 @@
+// INSTRUCTIONS:
+
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+
+// MY SOLUTION
+
+function greet (name, owner) {
+    return name === owner ? 'Hello boss' : 'Hello guest';
+  }
